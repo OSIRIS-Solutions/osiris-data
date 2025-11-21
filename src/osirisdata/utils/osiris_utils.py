22a75ea24179a9380c0df6bde955ec7b4170149a
juwitte <juwi93@gmx.de>
2025-11-21 15:37:00 +0100

    progress

--- src/osirisdata/utils/osiris_utils.py
+++ src/osirisdata/utils/osiris_utils.py
@@ -4,19 +4,26 @@ from typing import Annotated
 from pydantic import BaseModel, Field, ValidationError, create_model
 
 
-class AuthorPosition(str, Enum):
-    FIRST = 'first'
-    MIDDLE = 'middle'
-    LAST = 'last'
+class Position(str, Enum):
+    FIRST = "first"
+    MIDDLE = "middle"
+    LAST = "last"
 
-class Author(BaseModel):
-    last : str
-    first : str
-    aoi : bool
-    position : AuthorPosition
-    user : str | None = None
-    approved : bool | None = None
-    sws : int | None = None
+class Gender(str, Enum):
+    DIVERSE = "d"
+    FEMALE = "f"
+    MALE = "m"
+    NOINFO = "-"
+
+class Person(BaseModel):
+    last: str
+    first: str
+    aoi: bool
+    position: Position
+    user: str | None = None
+    approved: bool | None = None
+    sws: int | None = None
+
 
 class SplitDate(BaseModel):
     year: Annotated[int, Field(gt=1800, lt=2100)]
@@ -24,51 +31,37 @@ class SplitDate(BaseModel):
     day: Annotated[int, Field(ge=1, le=31)]
 
 
-
 MODULES = {
-    "authors": {
-        "authors": Annotated[list[Author], Field(min_length=1)]
-    },
-    "author-table": {
-        "authors": Annotated[list[Author], Field(min_length=1)]
-    },
-    "book-series" : {
-        "series": (str | None)
-    },
-    "book-title" : {
-        "book": (str | None)
-    },
-    "city": {
-        "city": (str | None)
-    },
-    "conference": {
-        "conference": (str | None)
-    },
-    "correction": {
-        "correction": (bool | None)
-    },
-    "date-range": {
-        "start": SplitDate,
-        "end": SplitDate
-    },
-    "date-range-ongoing":{
-        "start": SplitDate,
-        "end": None
-    },
+    "authors": {"authors": Annotated[list[Person], Field(min_length=1)]},
+    "author-table": {"authors": Annotated[list[Person], Field(min_length=1)]},
+    "book-series": {"series": (str | None)},
+    "book-title": {"book": (str | None)},
+    "city": {"city": (str | None)},
+    "conference": {"conference": (str | None)},
+    "correction": {"correction": (bool | None)},
+    "date-range": {"start": SplitDate, "end": SplitDate},
+    "date-range-ongoing": {"start": SplitDate, "end": None},
     "date": {
-        f_name: Annotated[f_info.asdict()['annotation'] | None, *f_info.asdict()['metadata'], Field(**f_info.asdict()['attributes'])]
+        f_name: Annotated[
+            f_info.asdict()["annotation"] | None,
+            *f_info.asdict()["metadata"],
+            Field(**f_info.asdict()["attributes"]),
+        ]
         for f_name, f_info in SplitDate.model_fields.items()
     },
-    "details":{
-        "details": (str | None)
-    },
-    "doctype": {
-        "doc_type": (str | None)
-    },
-    "doi":{
-        "str": Annotated[str, Field()]
-    }
+    "details": {"details": (str | None)},
+    "doctype": {"doc_type": (str | None)},
+    "doi": {"str": Annotated[str, Field(pattern="^10\..*")]},
+    "edition": {"edition": Annotated[int, Field(ge=1)]},
+    "editors": {"editors": list[Person]},
+    "editorial": {"editor_type": (str | None)},
+    "event-select": {},
+    "funding_type": {},
+    "guest-category": { "category": (str | None)},
+    "gender": (Gender | None),
+    "nationality": {"country": Annotated[str, Field()]}
 }
+
 
 def build_validator(key, modules: list[str], field_dict):
     data_structure = {}
@@ -84,8 +77,7 @@ def build_validator(key, modules: list[str], field_dict):
     return create_model(key, **data_structure)
 
 
-
-def getValidators(types: list[dict], fields:list[dict]) -> dict:
+def getValidators(types: list[dict], fields: list[dict]) -> dict:
     validators = {}
     field_dict = {field["id"]: field["format"] for field in fields}
     for t in types:
